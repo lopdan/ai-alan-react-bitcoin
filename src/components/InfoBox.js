6e3c4d85aa56dc875595546d953ab935bde5c33f
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-08-22 23:11:51 +0200

    Minor unfinished changes InfoBox

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./InfoBox.css";
 
 const InfoBox = () => {
@@ -10,14 +10,13 @@ const InfoBox = () => {
 	})
 	const dataChangeHandler = e => {
 		setData({...data, [e.target.name]: e.target.value})
-	}
+	}	
 
-	const componentDidMount = e => {
-		const { data } = this.props;
+	const loadPriceDate = () => {
 		const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
 		fetch(url).then((r) => r.json()).then((bitcoinData) => {
 			const price = bitcoinData.bpi.USD.rate_float;
-			const change = price - data[0].y;
+			//const change = price - data[0].y;
 			const changeP = ((price - data[0].y) / data[0].y) * 100;
 
 			dataChangeHandler({
@@ -29,31 +28,37 @@ const InfoBox = () => {
 			console.log(e);
 		});
 	}
-	<div id="data-container">
-		{data.currentPrice ? (
-			<div id="left" className="box">
-			<div className="heading">
-				{data.currentPrice.toLocaleString("us-EN", {
-					style: "currency",
-					currency: "USD",
-				})}
+	/*useEffect(() => {
+		loadPriceDate();
+	}, [count]);*/
+
+	return(
+		<div id="data-container" loadPriceDate={() => void 0}>
+			{data.currentPrice ? (
+				<div id="left" className="box">
+				<div className="heading">
+					{data.currentPrice.toLocaleString("us-EN", {
+						style: "currency",
+						currency: "USD",
+					})}
+				</div>
+				<div className="subtext">
+					{"Updated X"}
+				</div>
+				</div>
+			) : null}
+			{data.currentPrice ? (
+				<div id="middle" className="box">
+				<div className="heading">{data.periodChangeD}</div>
+				<div className="subtext">Change Since Last X (USD)</div>
+				</div>
+			) : null}
+			<div id="right" className="box">
+				<div className="heading">{data.periodChangeP}</div>
+				<div className="subtext">Change Since Last X (%)</div>
 			</div>
-			<div className="subtext">
-				{"Updated X"}
-			</div>
-			</div>
-		) : null}
-		{data.currentPrice ? (
-			<div id="middle" className="box">
-			<div className="heading">{data.periodChangeD}</div>
-			<div className="subtext">Change Since Last X (USD)</div>
-			</div>
-		) : null}
-		<div id="right" className="box">
-			<div className="heading">{data.periodChangeP}</div>
-			<div className="subtext">Change Since Last X (%)</div>
 		</div>
-	</div>
+	);
   
 }
 
