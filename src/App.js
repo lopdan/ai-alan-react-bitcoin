933cf78edb211500de1d82efe3dcd8386e31f038
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-08-23 21:02:48 +0200

    Hooks fixed but fetch API needs to be changed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,84 @@
-import React, { useEffect } from 'react';
 import alanButton from '@alan-ai/alan-sdk-web';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import './App.css';
 
 import InfoBox from './components/InfoBox';
 
 const alan_SDK_key = 'c1344b0e5cda4dfcd27bc949d44adcca2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+  const[dataValues, updateData] = useState({
+    fetchingData: true,
+    data: null,
+    hoverLoc: null,
+    activePoint: null
+  })
+	useEffect(() => {
+		alanButton({
+			key: alan_SDK_key,
+			onCommand: ({ command }) => {
+				if(command === 'testCommand'){
+					alert('Test succesful');
+				}
+			}
+		})
+	}, [])
 
-    useEffect(() => {
-        alanButton({
-            key: alan_SDK_key,
-            onCommand: ({ command }) => {
-                if(command === 'testCommand'){
-                    alert('Test succesful');
-                }
+  useEffect(() => {
+    const getData = () => {
+        const url = 'https://api.coindesk.com/v1/bpi/historical/close.json';  
+        fetch(url).then(r => r.json())
+          .then((bitcoinData) => {
+            const sortedData = [];
+            let count = 0;
+            for (let date in bitcoinData.bpi){
+              sortedData.push({
+                d: moment(date).format('MMM DD'),
+                p: bitcoinData.bpi[date].toLocaleString('us-EN',{ style: 'currency', currency: 'USD' }),
+                x: count, //previous days
+                y: bitcoinData.bpi[date] // numerical price
+              });
+              count++;
             }
-        })
-    }, [])
-    
-    return(
-        <div>
-            <h1>Initial structure</h1>
-            <InfoBox/>
-        </div>
-    );
+            updateData({
+                data: sortedData,
+                fetchingData: false
+            })
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      }
+	  console.log(dataValues);
+      getData();
+  },[dataValues.activePoint])
 
+    return (
+
+      <div className='container'>
+        <div className='row'>
+          <h1>30 Day Bitcoin Price Chart</h1>
+        </div>
+        <div className='row'>
+          { !dataValues.fetchingData ?
+          <InfoBox data={dataValues.data} />
+          : null }
+        </div>
+        <div className='row'>
+          <div className='popup'>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='chart'>
+          </div>
+        </div>
+        <div className='row'>
+          <div id="coindesk"> Powered by <a href="http://www.coindesk.com/price/">CoinDesk</a></div>
+        </div>
+      </div>
+
+    );
 }
 
 export default App;
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,43 +1,54 @@
 import React, { useState, useEffect } from "react";
 import "./InfoBox.css";
 
-const InfoBox = () => {
-	const [data , setData] = useState({
+const InfoBox = props => {
+	const [profileState, setProfileState] = useState(props);
+	const [dataPrices , setData] = useState({
 		currentPrice: null,
 		periodChangeD: null,
 		periodChangeP: null,
-		//updatedAt: null,	
+		updatedAt: null	
 	})
 	const dataChangeHandler = e => {
-		setData({...data, [e.target.name]: e.target.value})
+		setData({...dataPrices, [e.target.name]: e.target.value})
 	}	
 
-	const loadPriceDate = () => {
-		const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
-		fetch(url).then((r) => r.json()).then((bitcoinData) => {
-			const price = bitcoinData.bpi.USD.rate_float;
-			//const change = price - data[0].y;
-			const changeP = ((price - data[0].y) / data[0].y) * 100;
+	useEffect(() => {
+		const getData = () => {
+			console.log(profileState);
+			const {data} = profileState.data;
+			const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+			fetch(url).then((r) => r.json()).then((bitcoinData) => {
+				const price = bitcoinData.bpi.USD.rate_float;
+				const change = price - data[0].y;
+				const changeP = ((price - data[0].y) / data[0].y) * 100;
 
-			dataChangeHandler({
-				currentPrice : bitcoinData.bpi.USD.rate_float,
-				periodChangeD: '',
-				periodChangeP: changeP.toFixed(2) + "%",
-			})
-		}).catch((e) => {
-			console.log(e);
-		});
-	}
-	/*useEffect(() => {
-		loadPriceDate();
-	}, [count]);*/
+				/*dataChangeHandler({
+					currentPrice : bitcoinData.bpi.USD.rate_float,
+					periodChangeD: '',
+					periodChangeP: changeP.toFixed(2) + "%",
+				})*/
+				setData({
+					currentPrice : bitcoinData.bpi.USD.rate_float,
+					periodChangeD: '',
+					periodChangeP: changeP.toFixed(2) + "%",
+				})
 
+				console.log(price);
+			}).catch(err => {
+				console.log(err);
+			});
+		}
+		console.log(dataPrices);
+		getData();
+	}, [dataPrices]);
+	
 	return(
 		<div id="data-container" loadPriceDate={() => void 0}>
-			{data.currentPrice ? (
+			{dataPrices.currentPrice ? (
 				<div id="left" className="box">
 				<div className="heading">
-					{data.currentPrice.toLocaleString("us-EN", {
+					{dataPrices.currentPrice.toLocaleString("us-EN", {
 						style: "currency",
 						currency: "USD",
 					})}
@@ -47,14 +58,14 @@ const InfoBox = () => {
 				</div>
 				</div>
 			) : null}
-			{data.currentPrice ? (
+			{dataPrices.currentPrice ? (
 				<div id="middle" className="box">
-				<div className="heading">{data.periodChangeD}</div>
+				<div className="heading">{dataPrices.periodChangeD}</div>
 				<div className="subtext">Change Since Last X (USD)</div>
 				</div>
 			) : null}
 			<div id="right" className="box">
-				<div className="heading">{data.periodChangeP}</div>
+				<div className="heading">{dataPrices.periodChangeP}</div>
 				<div className="subtext">Change Since Last X (%)</div>
 			</div>
 		</div>
